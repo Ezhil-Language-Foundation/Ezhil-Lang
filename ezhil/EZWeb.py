7aae38631dd800ef36b190408197aa0c26e40fd8
Muthiah Annamalai <muthuspost@gmail.com>
2013-07-05 02:23:25 -0400

    EZWeb.py : Ezhil on the web - thin-client HTTP server - interface using a GET request as, http://hostname/?eval=true&program=EZHILPROGRAM ezhil.py : update to support FileExecuter, InterpExecuter classes. Add a local_REPL method for EZWeb based interfaces tests still at 57/5 pass/fail

---
+++ ezhil/EZWeb.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+## 
+## (C) 2013 Muthiah Annamalai,
+## Licensed under GPL Version 3
+## 
+## Interpreter for Ezhil language on the web
+
+## Ref: http://wiki.python.org/moin/BaseHttpServer
+
+import time
+from ezhil import EzhilFileExecuter, EzhilInterpExecuter
+import BaseHTTPServer, tempfile
+from os import unlink
+import cgi
+
+class EzhilOnTheWeb(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):        
+        print str(self.headers)
+        if self.path.find('/ezhil') >= 0:
+            GETvars = cgi.parse_qs( self.path )
+            print str(GETvars)
+            if GETvars.has_key('prog'):
+                program = "".join(GETvars['prog'])
+            else:
+                program = 'printf("Welcome to Ezhil! You can type a program and see its output here!")\n'
+            # add exit symbol
+            #program = program + "\n exit()";
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.do_ezhil_execute( program )
+        else: 
+            self.send_error(404)
+        return
+    
+    def do_ezhil_execute(self,program):
+        # write the input program into a temporary file and execute the Ezhil Interpreter
+        tmpf=tempfile.NamedTemporaryFile(suffix='.n',delete=False)
+        tmpf.write(program)
+        tmpf.close()
+
+        print( "Source program" )
+        print( open(tmpf.name).read() )
+        print( "*"*60 )
+        
+        # run the interpreter in a sandbox and capture the output hopefully
+        try:
+            failed = False
+            #obj = EzhilFileExecuter( file_input = tmpf.name, redirectop = True )
+            obj = EzhilInterpExecuter( file_input = tmpf.name, redirectop = True )
+            progout = obj.get_output()
+            op = "<B>Succeeded Execution</B> for program <font color=\"blue\"><pre>%s</pre></font> as <br/> <font color=\"green\"><pre>%s</pre></font>"%(program,progout)
+        except Exception as e:
+            print str(e)
+            failed = True
+            op = "<B>FAILED Execution</B> for program <font color=\"blue\"><pre>%s</pre></font> with <font color=\"red\">error <pre>%s</pre> </font>"%(program,str(e))
+        else:
+            print "Output file"
+            obj.get_output()
+        
+        # delete the temporary file
+        unlink(tmpf.name)
+        
+        if failed:
+            op = "<H2> Your program has some errors! Try correcting it and re-evaluate the code</H2><HR/><BR/>"+op
+        else:
+            op = "<H2> Your program executed correctly! Congratulations. </H2><HR/><BR/>"+op            
+        
+        self.wfile.write("<html> <head> <title>Ezhil interpreter</title> </head><body> %s </body></html>\n"%op)
+        
+        return op
+
+HOST_NAME = "localhost"
+PORT_NUMBER = 8080
+
+if __name__ == "__main__":
+    httpd = BaseHTTPServer.HTTPServer((HOST_NAME, PORT_NUMBER), EzhilOnTheWeb)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
--- ezhil/ezhil.py
+++ ezhil/ezhil.py
@@ -6,7 +6,7 @@
 ## 
 ## Interpreter for Ezhil language
 
-import os, sys, string, inspect
+import os, sys, string, tempfile
 from Interpreter import Interpreter, REPL, Lex, get_prog_name
 from ezhil_parser import EzhilParser
 from ezhil_scanner import EzhilLex
@@ -22,12 +22,12 @@ class EzhilInterpreter( Interpreter ):
         Interpreter.install_builtins(self)
         
         #input statements, length constructs
-        tamil_equiv = {"சரம்_இடமாற்று":"replace",            "சரம்_கண்டுபிடி":"find",         "நீளம்":"len",
-                                "சரம்_உள்ளீடு":"raw_input",       "உள்ளீடு" : "input" }
+        tamil_equiv = {"சரம்_இடமாற்று":"replace", "சரம்_கண்டுபிடி":"find","நீளம்":"len",
+                       "சரம்_உள்ளீடு":"raw_input", "உள்ளீடு" : "input" }
         #list operators        
         tamil_equiv.update( {"பட்டியல்":"list","பின்இணை":"append","தலைகீழ்":"reverse",
-                                         "வரிசைப்படுத்து":"sort","நீட்டிக்க":"extend","நுழைக்க":"insert","குறியீட்டெண்":"index",
-                                         "வெளியேஎடு":"pop","பொருந்தியஎண்":"count", "எடு":"get"} )
+                             "வரிசைப்படுத்து":"sort","நீட்டிக்க":"extend","நுழைக்க":"insert","குறியீட்டெண்":"index",
+                             "வெளியேஎடு":"pop","பொருந்தியஎண்":"count", "எடு":"get"} )
         
         #file operators
         tamil_equiv.update({"கோப்பை_திற":"file_open", "கோப்பை_மூடு":"file_close","கோப்பை_படி":"file_read",
@@ -45,7 +45,135 @@ class EzhilInterpreter( Interpreter ):
             self.builtin_map[k] = self.builtin_map[vv]
         
         return
+
+class EzhilRedirectOutput:
+    """ class provides the get_output method for reading from a temporary file, and deletes it after that.
+        the file creation is also managed here. However restoring stdout, stderr have to be done in the user class
+    """
+    def __init__(self,redirectop):
+        self.op = None
+        self.redirectop = redirectop
+
+        self.tmpf = None
+        if ( self.redirectop ):
+            self.tmpf=tempfile.NamedTemporaryFile(suffix='.output',delete=False)
+            self.old_stdout = sys.stdout
+            self.old_stderr = sys.stderr
+            sys.stdout = self.tmpf
+            sys.stderr = self.tmpf
+        pass
     
+        
+    
+    def get_output( self ):
+        """ read the output from tmpfile once and delete it. Use cached copy for later. Memoized. """ 
+        if ( not isinstance(self.op,str) ):
+            self.op = ""
+            if ( self.redirectop ):
+                with open(self.tmpf.name) as fp:
+                    self.op = fp.read()
+                os.unlink( self.tmpf.name )
+                self.tmpf = None
+        
+        return self.op
+
+
+class EzhilRedirectInputOutput(EzhilRedirectOutput):
+    def __init__(self,input_file,redirectop):
+        EzhilRedirectOutput.__init__(self,redirectop)
+        self.old_stdin = sys.stdin
+        self.stdin = open( input_file )
+    
+
+class EzhilFileExecuter(EzhilRedirectOutput):
+    """ run on construction - build a Ezhil lexer/parser/runtime and execute the file pointed to by @files """
+    def __init__(self,file_input,debug=False,redirectop=False):        
+        EzhilRedirectOutput.__init__(self,redirectop)
+        
+        try:
+            lexer = EzhilLex(file_input)
+            if ( debug ): lexer.dump_tokens()        
+            parse_eval = EzhilInterpreter( lexer, debug )
+            parse_eval.parse()
+            if ( debug ):  print("*"*60);  print(str(parse_eval))
+            env = parse_eval.evaluate()
+        except Exception as e:            
+            print("exception ",str(e))
+            raise e
+        finally:
+            if ( redirectop ):
+                self.tmpf.close()
+                sys.stdout = self.old_stdout
+                sys.stderr = self.old_stderr
+        
+
+def local_REPL( file_input, lang, lexer, parse_eval, debug=False):    
+    #refactor out REPL for ezhil and exprs
+    env = None ## get the first instance from evaluate_interactive
+    line_no = 1
+    do_quit = False
+        ## world-famous REPL
+    with open(file_input) as fp:
+        lines = fp.readlines()
+    for buffer in lines:
+        try:
+            sys.stdout.write("%s %d> "%(lang,line_no))
+            ## FIXME: implement multiple line readline library
+            buffer = buffer.strip()
+            line_no += 1
+            if ( buffer.strip() == 'exit' ):
+                do_quit = True
+        except EOFError as e:
+            print("End of Input reached\n")
+            do_quit = True ##evaluate the buffer 
+            ## line-no broke
+        if ( debug ): print("evaluating buffer", buffer)
+        if ( do_quit ):
+            if ( lang == 'ezhil' ):
+                print("******* வணக்கம்! பின்னர் உங்களை  பார்க்கலாம். *******") 
+            else:
+                print("******* Goodbye! Now have a nice day *******") 
+            return
+        try:
+            lexer.set_line_col([line_no, 0])
+            lexer.tokenize(buffer)
+            [line_no,c] = lexer.get_line_col( 0 )
+            if ( debug ): lexer.dump_tokens()
+            parse_eval.parse()
+            if ( debug ):  print("*"*60);  print(str(parse_eval))
+            [rval, env] = parse_eval.evaluate_interactive(env)
+            if hasattr( rval, 'evaluate' ):
+                print(rval.__str__())
+            else:
+                print(rval)
+        except Exception as e:
+            print(e)
+            ## clear tokens in lexer
+            lexer.tokens = list()
+            raise e
+    return
+
+
+class EzhilInterpExecuter(EzhilRedirectInputOutput):
+    """ run on construction - build a Ezhil lexer/parser/runtime and execute the file pointed to by @files """
+    def __init__(self,file_input,debug=False,redirectop=False):
+        EzhilRedirectInputOutput.__init__(self,file_input,redirectop)
+        
+        try:
+            lang = "எழில்"
+            lexer = EzhilLex( )
+            parse_eval = EzhilInterpreter( lexer, debug )
+            local_REPL( file_input, lang, lexer, parse_eval, debug )
+        except Exception as e:            
+            print("exception ",str(e))
+            raise e
+        finally:
+            if ( redirectop ):
+                self.tmpf.close()
+                sys.stdout = self.old_stdout
+                sys.stderr = self.old_stderr
+                sys.stdin = self.old_stdin
+
 if __name__ == "__main__":
     lang = "எழில்"
     [fname, debug, dostdin ]= get_prog_name(lang)
@@ -57,11 +185,5 @@ if __name__ == "__main__":
     else:
         ## evaluate a files sequentially
         for files in fname:
-            lexer = EzhilLex(files)
-            if ( debug ): lexer.dump_tokens()
-            parse_eval = EzhilInterpreter( lexer, debug )
-            parse_eval.parse()
-            if ( debug ):  print("*"*60);  print(str(parse_eval))
-            env = parse_eval.evaluate()
-        
+            EzhilFileExecuter( files )
     pass
