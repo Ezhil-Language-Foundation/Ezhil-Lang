997f826dbe5220b2bc76db2564824cbda3e7b26a
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-08-25 10:48:36 -0400

    -x

--- ancilla/update_website.sh
+++ ancilla/update_website.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Copying HTML files"
 sudo cp ./website/* /var/www/html/
-sudo cp ./web/ezhil_eval.html /var/www/html
+sudo cp ./website/ezhil_eval.html /var/www/html
 sudo cp ./web/ezhil_web.py /var/www/cgi-bin
 echo "Restart httpd"
 sudo service httpd restart
