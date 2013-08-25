74fa285e9cb812eed45431e0058d5b81e7adf642
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-08-25 03:25:12 -0400

    update website

--- update_website.sh
+++ update_website.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Copying HTML files"
 sudo cp ./website/* /var/www/html/
+sudo cp ./web/ezhil_eval.html /var/www/html
+sudo cp ./web/ezhil_web.py /var/www/cgi-bin
 echo "Restart httpd"
 sudo service httpd restart
 
