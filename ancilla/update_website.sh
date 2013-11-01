43a9cdc6c05b5d75234596a81f71474b412eb2d9
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-11-01 02:37:25 -0400

    rel paths update

--- ancilla/update_website.sh
+++ ancilla/update_website.sh
@@ -2,6 +2,7 @@
 echo "Copying HTML files"
 sudo cp ./website/* /var/www/html/
 sudo cp ./website/ezhil_eval.html /var/www/html
+sudo cp -ar ./ezhil_tests /var/www/html/
 sudo cp ./web/ezhil_web.py /var/www/cgi-bin
 echo "Restart httpd"
 sudo service httpd restart
