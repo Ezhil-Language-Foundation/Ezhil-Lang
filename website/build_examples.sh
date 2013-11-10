947cda947534deb5e9f72ddb0ec07e42efda076a
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-11-10 12:01:37 -0500

    remove example.txt, add some frills to build script

--- website/build_examples.sh
+++ website/build_examples.sh
@@ -5,5 +5,7 @@
 ls ../ezhil_tests/*.n -c1 | cut -d'/' -f3 | sort -u > examples.txt
 for i in `cat examples.txt`
 do 
-  echo '<option value="'"$i"'">'$i'</option>;' 
+  echo '<option value="'"$i"'">'$i'</option>' 
 done
+echo "we have a total of " `wc -l examples.txt` " to show the user on Ezhil website"
+rm examples.txt
