57f78cb3e840cfa7cfae0dc3e02a3b00a551d185
Muthiah Annamalai <arcturusannamalai@gmail.com>
2014-07-27 11:38:28 -0400

    bump v #. update credits.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 from codecs import open
 
 setup(name='ezhil',
-      version='0.75-dev-update',
+      version='0.76',
       description='Ezhil - Tamil programming language implemented in Python',
       author='Muthiah Annamalai',
       author_email='ezhillang@gmail.com',
