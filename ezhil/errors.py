dc22d0252718fad362f8f8f07f4c2dc177b6aeab
Muthiah Annamalai <ezhillang@gmail.com>
2015-01-27 12:52:32 -0500

    1) update ezhil scanner to prevent identifiers like b@bb@ge, Li$kov, and Gr@ce etc. 2) Add unittest for #1 3) Update website with media coverage links 4) Update dot files 5) Unittest also prints a small banner

--- ezhil/errors.py
+++ ezhil/errors.py
@@ -6,6 +6,12 @@
 ## This module is the custom exceptions for
 ## the exprs language
 
+class Messages:
+    LEXICAL_ERROR = "Lexical error:"
+    RUNTIME_ERROR = "Run-time error:"
+    PARSE_ERROR = "Parse error:"
+    TIMEOUT_ERROR = "process exceeded timeout of  %s(seconds)"
+
 ## Exception states
 class ErrorException(Exception):
     def __repr__(self):
@@ -18,15 +24,17 @@ class ErrorException(Exception):
 
 class ScannerException(ErrorException):
     def __init__(self,desc):
-        self.args = ["Lexical error:", desc ]
+        self.args = [Messages.LEXICAL_ERROR, desc ]
+
 
 class RuntimeException(ErrorException):
     def __init__(self,desc):
-        self.args = ["Run-time error", desc ]
+        self.args = [Messages.RUNTIME_ERROR, desc ]
+
 
 class ParseException(ErrorException):
     def __init__(self,desc):
-        self.args = ["Parse error:", desc ]
+        self.args = [Messages.PARSE_ERROR, desc ]
 
 
 class TimeoutException(Exception):
@@ -35,4 +43,4 @@ class TimeoutException(Exception):
             self.timeout = timeout
 
         def __str__(self):
-            return "process exceeded timeout of " + str(self.timeout) + "s"
+            return Messages.TIMEOUT_ERROR%(self.timeout)
