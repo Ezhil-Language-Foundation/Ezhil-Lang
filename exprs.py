d10179a02947749bc4d419184f47bac7619c9fa7
Muthiah Annamalai <ezhillang@gmail.com>
2016-10-20 00:48:41 -0700

    exprs properly exported

---
+++ exprs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+## 
+## (C) 2007-2016 Muthiah Annamalai,
+## Licensed under GPL Version 3
+import sys
+import codecs
+from ezhil import exprs_eval
+
+PYTHON3 = (sys.version[0] == '3')
+if PYTHON3:
+    unicode = str
+else:
+    sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+    #sys.stdin = codecs.getreader('utf-8')(sys.stdin)
+
+if __name__ == "__main__":
+    exprs_eval()
