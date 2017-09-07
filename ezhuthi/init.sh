ee6ae9d92b844ddac9a84db66bea1f20d158d3c7
Muthiah Annamalai <ezhillang@gmail.com>
2017-09-06 22:26:11 -0700

    fix ezhuthi packaging to include XMLBOOK folder from right location

--- ezhuthi/init.sh
+++ ezhuthi/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
 # Run this file to begin building ezhuthi package
 cp -ar ../editor ./ezhuthi
+find ./ -iname '*~' | xargs rm -vf
 # after this step you can run usual
 # python setup.py sdist build
 # python setup.py sdist install
