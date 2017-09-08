619aa80b728b3c55c2db0b58abec19cdf269120e
Muthiah Annamalai <ezhillang@gmail.com>
2017-09-07 21:47:52 -0700

    Fix syntax error in ezhuthi.py; Remove unused import of iyakki.py
    First cut at making the Ezhil Twitter bot
    Fix issue #203 - Twitter bot added

---
+++ ezhilbot/ezhilbot.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+# This file is part of Python-Twitter example @ https://github.com/bear/python-twitter
+# modified for being suitable to Ezhil-Language project needs.
+# See file COPYING.PYTHON-TWITTER in the source directory for the terms of use.
+# Typical use would be to run this program in a cron job.
+
+'''Post a message to twitter'''
+
+__author__ = ['dewitt@google.com','ezhillang@gmail.com']
+
+try:
+    import configparser
+except ImportError as _:
+    import ConfigParser as configparser
+import json
+import getopt
+import os
+import sys
+import twitter
+import codecs
+import random
+from iyakki import MPRunner
+
+USAGE = '''Usage: tweet [options] message
+
+  This script posts a message to Twitter.
+
+  Options:
+
+    -h --help : print this help
+    --consumer-key : the twitter consumer key
+    --consumer-secret : the twitter consumer secret
+    --access-key : the twitter access token key
+    --access-secret : the twitter access token secret
+    --encoding : the character set encoding used in input strings, e.g. "utf-8". [optional]
+
+  Documentation:
+
+  If either of the command line flags are not present, the environment
+  variables TWEETUSERNAME and TWEETPASSWORD will then be checked for your
+  consumer_key or consumer_secret, respectively.
+
+  If neither the command line flags nor the environment variables are
+  present, the .tweetrc file, if it exists, can be used to set the
+  default consumer_key and consumer_secret.  The file should contain the
+  following three lines, replacing *consumer_key* with your consumer key, and
+  *consumer_secret* with your consumer secret:
+
+  A skeletal .tweetrc file:
+
+    [Tweet]
+    consumer_key: *consumer_key*
+    consumer_secret: *consumer_password*
+    access_key: *access_key*
+    access_secret: *access_password*
+
+'''
+
+
+def PrintUsageAndExit():
+    print USAGE
+    sys.exit(2)
+
+
+def GetConsumerKeyEnv():
+    return os.environ.get("TWEETUSERNAME", None)
+
+
+def GetConsumerSecretEnv():
+    return os.environ.get("TWEETPASSWORD", None)
+
+
+def GetAccessKeyEnv():
+    return os.environ.get("TWEETACCESSKEY", None)
+
+
+def GetAccessSecretEnv():
+    return os.environ.get("TWEETACCESSSECRET", None)
+
+
+class TweetRc(object):
+    def __init__(self):
+        self._config = None
+
+    def GetConsumerKey(self):
+        return self._GetOption('consumer_key')
+
+    def GetConsumerSecret(self):
+        return self._GetOption('consumer_secret')
+
+    def GetAccessKey(self):
+        return self._GetOption('access_key')
+
+    def GetAccessSecret(self):
+        return self._GetOption('access_secret')
+
+    def _GetOption(self, option):
+        try:
+            return self._GetConfig().get('Tweet', option)
+        except:
+            return None
+
+    def _GetConfig(self):
+        if not self._config:
+            self._config = configparser.ConfigParser()
+            self._config.read(os.path.expanduser('~/.tweetrc'))
+        return self._config
+
+class LastReply:
+    FILENAME="ezhilbot.json"
+    
+    @staticmethod
+    def get_id():
+        with open(LastReply.FILENAME,"r") as fp:
+            data = json.loads(fp.read())
+        return int(data["refid"])
+    
+    @staticmethod
+    def set_id(id):
+        with open(LastReply.FILENAME,"w") as fp:
+            datas = json.dumps({"refid":id})
+            print(datas)
+            fp.write(datas)
+            fp.close()
+        return
+    
+    @staticmethod
+    def bootstrap():
+        try:
+            os.stat(LastReply.FILENAME)
+        except Exception as ioe:
+            LastReply.set_id(-1)
+    
+def main():
+    try:
+        shortflags = 'h'
+        longflags = ['help', 'consumer-key=', 'consumer-secret=',
+                     'access-key=', 'access-secret=', 'encoding=']
+        opts, args = getopt.gnu_getopt(sys.argv[1:], shortflags, longflags)
+    except getopt.GetoptError as ioe:
+         PrintUsageAndExit()
+    consumer_keyflag = None
+    consumer_secretflag = None
+    access_keyflag = None
+    access_secretflag = None
+    encoding = None
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            PrintUsageAndExit()
+        if o in ("--consumer-key"):
+            consumer_keyflag = a
+        if o in ("--consumer-secret"):
+            consumer_secretflag = a
+        if o in ("--access-key"):
+            access_keyflag = a
+        if o in ("--access-secret"):
+            access_secretflag = a
+        if o in ("--encoding"):
+            encoding = a
+    rc = TweetRc()
+    consumer_key = consumer_keyflag or GetConsumerKeyEnv() or rc.GetConsumerKey()
+    consumer_secret = consumer_secretflag or GetConsumerSecretEnv() or rc.GetConsumerSecret()
+    access_key = access_keyflag or GetAccessKeyEnv() or rc.GetAccessKey()
+    access_secret = access_secretflag or GetAccessSecretEnv() or rc.GetAccessSecret()
+    if not consumer_key or not consumer_secret or not access_key or not access_secret:
+        PrintUsageAndExit()
+    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
+                      access_token_key=access_key, access_token_secret=access_secret,
+                      input_encoding=encoding)
+    LastReply.bootstrap()
+    id = LastReply.get_id()
+    kwargs = {}
+    if id > 0:
+        kwargs = {"since_id":id}
+    rep = api.GetReplies(**kwargs)
+    for r in rep:
+        print("Reply : id=%s,text=%s,user=%s"%(r.id,r.text,r.user.screen_name))
+        print("%s"%r)
+        code_msg = r.text
+        user = r.user.screen_name
+        process_ezhil(api,code_msg,user)
+        LastReply.set_id(r.id)
+    else:
+        print("Nothing since last tweet of ID=%d"%LastReply.get_id())
+
+def process_ezhil(api,code_msg,user):
+    try:
+        srcfilename = "tmpcode_%d.n"%random.randint(0,10000)
+        with codecs.open(srcfilename,"w","UTF-8") as fp:
+            fp.write(code_msg)
+        runner = MPRunner(timeout=3) #run for 3s max.
+        runner.run(srcfilename)
+        runner.report()
+        message = unicode(runner)
+        message = u" ".join([u'@'+user,message])
+        
+        rev = []
+        # section in chunks of 140
+        for part in range(0,len(message),140):
+            rev.append(message[part:min(part+140,len(message))])
+        rev.reverse()
+        
+        for r in rev:
+            api.PostUpdate(r)
+        
+    except UnicodeDecodeError:
+        print(u"Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
+        print(u"Try explicitly specifying the encoding with the --encoding flag")
+        return
+    finally:
+        os.unlink(srcfilename)
+    print(u"%s just posted: %s" % (status.user.name, status.text))
+
+if __name__ == "__main__":
+    main()
--- ezhilbot/iyakki.py
+++ ezhilbot/iyakki.py
@@ -16,22 +16,15 @@ import tempfile
 import threading
 import time
 
-import gi
 import ezhil
 
 PYTHON3 = (sys.version[0] == '3')
 if PYTHON3:
     unicode = str
 
-gi.require_version('Gtk','3.0')
-
-from gi.repository import Gtk, GObject, GLib, Pango
-
 def MPRunner_actor(pipe,filename):
     multiprocessing.freeze_support()
-    GObject.threads_init()
     is_success = False
-    ezhil.EzhilCustomFunction.set(GtkStaticWindow.dummy_input)
     old_stdin = sys.stdin
     old_stdout = sys.stdout
     old_stderr = sys.stderr
@@ -42,7 +35,7 @@ def MPRunner_actor(pipe,filename):
     try:
         sys.stdout = codecs.open(tmpfilename,"w","utf-8")
         sys.stderr = sys.stdout
-        executer = ezhil.EzhilFileExecuter(filename)
+        executer = ezhil.EzhilFileExecuter(filename,debug=False,redirectop=False,TIMEOUT=3,encoding="utf-8",doprofile=False,safe_mode=True)
         executer.run()
         is_success = True
     except Exception as e:
@@ -89,56 +82,29 @@ class MPRunner:
             res_std_out = u"தெரியாத பிழை நேர்ந்தது!"
         self.res_std_out,self.is_success = res_std_out,is_success
         return
-
-class GtkStaticWindow:
-    _instance = None
-
-    @staticmethod
-    def get_instance():
-        if not GtkStaticWindow._instance:
-            GtkStaticWindow._instance = GtkStaticWindow()
-        return GtkStaticWindow._instance
-
-    def __init__(self):
-        self.gui = threading.Thread(target=lambda : Gtk.main_iteration())
         
-        self.window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
-        self.window.set_default_size(100,150) #vanishingly small size
-        self.window.connect("delete-event",Gtk.main_quit)
-        self.window.set_title(u"எழில் இயக்கி சாளரம்")
-        self.window.set_decorated(False)
-        self.window.show_all()
-        GLib.idle_add( Gtk.main_iteration )
-        self.gui.start()
-
-    @staticmethod
-    def dummy_input(*args):
-        message= not args and "Enter Input" or args[0]
-        if not args or len(args) < 2:
-            title = u"எழுதி - எழில் மொழி செயலி" #Ezhil language IDE
+    def __str__(self):
+        r = list()
+        if self.is_success:
+            r.append(u"SUCCESS")
         else:
-            title = args[1]
-        static_window = GtkStaticWindow.get_instance()
-        window = static_window.window
-        dialogWindow = Gtk.MessageDialog(window,
-                              Gtk.DialogFlags.MODAL,
-                              Gtk.MessageType.QUESTION,
-                              Gtk.ButtonsType.OK_CANCEL,
-                              message)
-        dialogWindow.set_title(title)
-        dialogBox = dialogWindow.get_content_area()
-        userEntry = Gtk.Entry()
-        userEntry.set_size_request(257,0)
-        dialogBox.pack_end(userEntry, False, False, 0)
-        dialogWindow.show_all()
-        response = dialogWindow.run()
-        text = userEntry.get_text() 
-        print(u"#####- உள்ளீடு நிரல்பாகம் இயங்கி முடிந்தது -####")
-        if (response == Gtk.ResponseType.OK) and (text != ''):
-            return text
-        return ""
+            r.append(u"FAILURE")
+            
+        r.append(u"\nOUTPUT:")
+        r.append(self.res_std_out)
+        return u" ".join(r)
+        
+    def report(self):
+        if self.is_success:
+            print("SUCCESS")
+        else:
+            print("FAILURE")
+            
+        print("OUTPUT:")
+        print(self.res_std_out)
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     runner = MPRunner()
     runner.run(filename)
+    runner.report()
